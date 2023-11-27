5def64855f0e9573b5b483cca97cf24f545d25f2
TomSadan <tomsadan2@gmail.com>
2023-11-26 19:02:56 -0500

    Reduced redundancy in models.py, added get_similarity_score method for VideoModel

--- libs/langchain/langchain/chains/video_captioning/models.py
+++ libs/langchain/langchain/chains/video_captioning/models.py
@@ -1,8 +1,7 @@
-class VideoModel:
-    def __init__(self, start_time, end_time, image_description):
+class BaseModel:
+    def __init__(self, start_time, end_time):
         self._start_time = start_time
         self._end_time = end_time
-        self._image_description = image_description
 
     @property
     def start_time(self):
@@ -19,6 +18,15 @@ class VideoModel:
     @end_time.setter
     def end_time(self, value):
         self._end_time = value
+
+    def __str__(self):
+        return f"start_time: {self.start_time}, end_time: {self.end_time}"
+
+
+class VideoModel(BaseModel):
+    def __init__(self, start_time, end_time, image_description):
+        super().__init__(start_time, end_time)
+        self._image_description = image_description
 
     @property
     def image_description(self):
@@ -29,30 +37,27 @@ class VideoModel:
         self._image_description = value
 
     def __str__(self):
-        return f"start_time: {self.start_time}, end_time: {self.end_time}, image_description: {self.image_description}"
+        return f"{super().__str__()}, image_description: {self.image_description}"
+    
+    def get_similarity_score(self, other):
+        # Tokenize the image descriptions by extracting individual words, stripping trailing 's' (plural = singular)
+        # and converting the words to lowercase in order to be case-insensitive
+        self_tokenized = set(word.lower().rstrip('s') for word in self.image_description.split())
+        other_tokenized = set(word.lower().rstrip('s') for word in other.image_description.split())
+
+        # Find common words
+        common_words = self_tokenized.intersection(other_tokenized)
+
+        # Calculate similarity score
+        similarity_score = len(common_words) / max(len(self_tokenized), len(other_tokenized)) * 100
+
+        return similarity_score
 
 
-class AudioModel:
+class AudioModel(BaseModel):
     def __init__(self, start_time, end_time, subtitle_text):
-        self._start_time = start_time
-        self._end_time = end_time
+        super().__init__(start_time, end_time)
         self._subtitle_text = subtitle_text
-
-    @property
-    def start_time(self):
-        return self._start_time
-
-    @start_time.setter
-    def start_time(self, value):
-        self._start_time = value
-
-    @property
-    def end_time(self):
-        return self._end_time
-
-    @end_time.setter
-    def end_time(self, value):
-        self._end_time = value
 
     @property
     def subtitle_text(self):
@@ -63,30 +68,13 @@ class AudioModel:
         self._subtitle_text = value
 
     def __str__(self):
-        return f"start_time: {self.start_time}, end_time: {self.end_time}, subtitle_text: {self.subtitle_text}"
+        return f"{super().__str__()}, subtitle_text: {self.subtitle_text}"
 
 
-class CaptionModel:
+class CaptionModel(BaseModel):
     def __init__(self, start_time, end_time, closed_caption):
-        self._start_time = start_time
-        self._end_time = end_time
+        super().__init__(start_time, end_time)
         self._closed_caption = closed_caption
-
-    @property
-    def start_time(self):
-        return self._start_time
-
-    @start_time.setter
-    def start_time(self, value):
-        self._start_time = value
-
-    @property
-    def end_time(self):
-        return self._end_time
-
-    @end_time.setter
-    def end_time(self, value):
-        self._end_time = value
 
     @property
     def closed_caption(self):
@@ -97,4 +85,4 @@ class CaptionModel:
         self._closed_caption = value
 
     def __str__(self):
-        return f"start_time: {self.start_time}, end_time: {self.end_time}, closed_caption: {self.closed_caption}"
+        return f"{super().__str__()}, closed_caption: {self.closed_caption}"
